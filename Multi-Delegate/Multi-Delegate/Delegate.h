b558bc51b7a0a82e613cb1191a0c59b4c1939a14
UDE\astrid.hsu <hsucc6351518@gmail.com>
2017-06-01 19:17:08 +0800

    delegate

--- Multi-Delegate/Multi-Delegate/Delegate.h
+++ Multi-Delegate/Multi-Delegate/Delegate.h
@@ -1,14 +1,58 @@
 #include <list>
-
-template <class Types> class Delegate
+#include <functional>
+template <class CType> 
+class Delegate
 {
 public:
 	//Types a;
 	Delegate();
 	//template <class Type>
-	//Delegate& operator+(Type &nextOperation);
+	Delegate& operator+(CType nextOperation)
+	{
+		_commandList.push_back(nextOperation);
+		return *this;
+	}
+
+	void Invoke();
 
 private:
-	Types _t;
-	//std::list<<Type> CommandList;
+	//CType _t;
+	std::list<CType> _commandList;
 };
+template <class CType>
+Delegate<CType>::Delegate()
+{
+	Invoke();
+}
+/*
+template <class CType>
+void Delegate<CType>::operator+(std::function<CType> &nextOperation)
+{
+	_commandList.push_back(nextOperation);
+	//return *this;
+}
+*/
+template <class CType>
+void Delegate<CType>::Invoke()
+{
+	//std::cout << _commandList.size();
+	//void(*function)();
+
+	for each (CType var in _commandList)
+	{
+		//var();
+	}
+
+	for (std::list<CType>::iterator i = _commandList.begin(); i != _commandList.end(); i++)
+	{
+	    (*i)();
+	}
+	//{
+//		CType temp = _commandList[i];
+		//std::function<CType> temp = _commandList[i];
+		//function = temp ;
+		//function();
+		//&_commandList[i];
+		//s();
+	//}
+}
--- Multi-Delegate/Multi-Delegate/Multi-Delegate.cpp
+++ Multi-Delegate/Multi-Delegate/Multi-Delegate.cpp
@@ -1,14 +1,34 @@
 // Multi-Delegate.cpp : Defines the entry point for the console application.
+// Win32 Application
+// Author: Astrid Hsu
+// Brief: Multi-Delegate sample code
+// Date: 2017-06-01
 //
 
 #include "stdafx.h"
 #include <iostream>
 #include "Delegate.h"
-
+typedef int(*function)();
+int funct1()
+{
+	std::cout << "I am funct1" << std::endl;
+	return 1;
+}
+int funct2()
+{
+	std::cout << "I am funct2" << std::endl;
+	return 2;
+}
 int main()
 {
-	Delegate<int> a;
+	Delegate<function> a;
 
+	//function = &funct1;
+	a + funct1;
+	//a+function;
+	//function = &funct2;
+	a+funct2;
+	a.Invoke();
 	system("pause");
 	return 0;
 
